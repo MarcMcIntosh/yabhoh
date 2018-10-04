2989a7b0170afe0630256db14e58176472e3ebee
Marc McIntosh <Marc.McIntosh@holidayCheck.com>
2018-10-04 11:01:44 +0200

    Added more directories to bootstrap-script

--- scripts/bootstrap_directories.sh
+++ scripts/bootstrap_directories.sh
@@ -15,22 +15,23 @@ LEARNING=learning;
 mkdir public
 
 ## TESTS DIRECTORY
-mkdir -p resources/tests/{tdd,tfp,headless-browser-testing,visual-regression-testing};
+mkdir -p resources/tests/{tfp-tdd,pyramid,types,unit,intergrated,headless-browser,visual-regression,techniques};
+
 
 ## TOOLS DIRECTORY
-mkdir -p resources/tools/{bash,vim,nix,docker,travis,jenkins,prometheus}
+mkdir -p resources/tools/{bash,vim,git-github,nix,docker,travis,jenkins,prometheus}
 
 ## AGILE DIRECTORY
-mkdir -p resources/agile
+mkdir -p resources/agile-methods/{scrum,xp,moderation,facilitation,tools}
 
 ## SOFTWARE DESIGN
-mkdir -p resources/software-design
+mkdir -p resources/software-design/{fp,oop,solid,kiss,dry,yagni,sql-and-nosql,monolith-and-mirco-services,monorepo-and-multirepo}
 
 ## COMMUNICATION
-mkdir -p resources/communication
+mkdir -p resources/communication/{public-speaking,blogging,confrences-and-meetups,free-software,netiquette}
 
 ## LEARNING
-mkdir -p resources/learning
+mkdir -p resources/learning/{books-to-read,learn-by-teaching}
 
 ## Create Readme files
 find resources/ -type d -exec touch {}/README.md \;
